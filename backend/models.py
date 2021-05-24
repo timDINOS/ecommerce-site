f5df85cf318c0b22320637063ba99af1935a4477
timDINOS <tchoi720@gmail.com>
2021-05-23 22:02:03 -0700

    working on model

--- backend/models.py
+++ backend/models.py
@@ -4,11 +4,11 @@ from django.db.models import Sum;
 
 
 class AccountProfile(models.Model):
+    my_user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pay_with_click = models.BooleanField(default=False)
+
     def _str_(self):
-       return
-     
-class Payment(models.Model):
-    pass;
+       return self.my_user
 
 
 class Item(models.Model):
@@ -18,7 +18,29 @@ class Item(models.Model):
         return self.name_of_item
 
 
+class Payment(models.Model):
+    my_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    order_item = models.BooleanField(default=False)
+    given_item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    number_of_given_item = models.IntegerField(default=1)
+
+    def get_agg_price(self):
+        return self.number_of_given_item * self.given_item.price"
+
+    def _str_(self):
+        return f"{self.number_of_given_item} of {self.item.name_of_item}"
+    
+
+
+
 class Coupon(models.Model):
-    pass;
+    value = models.CharField(max_length=100)
+    amt_of_coupons = models.FloatField()
+
+    def _str_(self):
+        return self.code
+
+
+
 
 
